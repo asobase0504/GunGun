6cc1da6ea3b60ba813c0994e13086f0282d342f2
湯田海都 <211019@st.yoshida-g.ac.jp>
2022-01-12 10:46:57 +0900

    線の設定をline.cppに移行

--- Line.h
+++ Line.h
@@ -21,5 +21,6 @@ void UpdateLine(void);
 void DrawLine(void);
 void SetLine(D3DXVECTOR3* pos, D3DXVECTOR3* rot, D3DXVECTOR3 start, D3DXVECTOR3 end, D3DXCOLOR col);
 void SetLine(D3DXVECTOR3* pos, D3DXQUATERNION* quaternion, D3DXVECTOR3 start, D3DXVECTOR3 end, D3DXCOLOR col);
+void SetModelLine(D3DXVECTOR3* pos, D3DXQUATERNION* quaternion, D3DXVECTOR3 Max, D3DXVECTOR3 Min, D3DXCOLOR col);
 
 #endif // !_LINE_H_
